1bda6a19248d696569eeec8ccf2c2bb785b0dc1d
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-24 15:56:49 -0300

    dealing with strings

---
+++ DealingWithStrings.rb
@@ -0,0 +1,12 @@
+class DealingWithStrings
+    def initialize()
+    end
+    def operations()
+        result = 2+3
+        prints "The result of this addition is = #{result}"
+    end
+end
+
+object = DealingWithStrings.new()
+object.operations
+gets()
