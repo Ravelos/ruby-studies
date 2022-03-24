f3c89b3d213a076ca41e90a7095c4ac27f390abb
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-24 12:27:53 -0300

    my first class with ruby

--- hola.rb
+++ hola.rb
@@ -1,3 +1,12 @@
-puts "Hola mundo"
+class HelloWorld
+    def initialize()
+    end
+    def greet()
+        greeting = "Hello world"
+        puts greeting
+    end
+end
 
+object = HelloWorld.new()
+object.greet
 gets()
