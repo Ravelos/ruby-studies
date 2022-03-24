81fd64f14f302b45fd70834210d70445226df59f
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-24 14:57:50 -0300

    showing results

---
+++ mathematicalOperators.rb
@@ -0,0 +1,19 @@
+class ArithmethicOperations
+    def initialize()
+    end
+    def operations()
+      addition = 2+1
+      substraction = 3-2
+      multiplication = 3*3
+      division = 3/2
+
+      puts addition
+      puts substraction
+      puts multiplication
+      puts division
+    end
+end
+
+object = ArithmethicOperations.new()
+object.operations
+gets()
