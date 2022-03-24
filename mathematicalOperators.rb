afea1a9fded8070a038190111dc7952724c5bc48
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-24 15:17:59 -0300

    showing results

--- mathematicalOperators.rb
+++ mathematicalOperators.rb
@@ -8,11 +8,16 @@ class ArithmethicOperations
       division = 3/2
       exponent = 2**3
 
+      #Using a variable
+
+      variable = (10+20)/(3*2)
+
       puts addition
       puts substraction
       puts multiplication
       puts division
       puts exponent
+      puts variable
     end
 end
 
