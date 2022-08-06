2b082a0ddc0b6d7390b902d8a068bad91f3a07e9
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-08-05 23:02:54 -0300

    practicing the comments

--- 2-input-and-output.rb
+++ 2-input-and-output.rb
@@ -1,7 +1,13 @@
 puts "Hello"
 puts "Write your name"
-n1 = gets.chomp
+n1 = gets.chomp #This is the way to get and input
 puts "Your name is " + n1
 puts "Write your age"
 age = gets.chomp
 puts "Your age is " + age
+
+# This is a comment
+=begin
+
+    This is another comment
+=end
