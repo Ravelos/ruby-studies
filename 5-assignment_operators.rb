058112b206c304492ae0c46ef1db4dea0e31a0cc
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-08-06 09:21:51 -0300

    assigment operators

---
+++ 5-assignment_operators.rb
@@ -0,0 +1,11 @@
+#Assignment operators
+a =4
+a = a + 1 #There is an easier way to write this with += operator
+a += 1
+
+a -= 1 # a = a - 1
+a *= 1 # a = a * 1
+a /= 1 # a = a / 1
+a %= 1 # a = a % 1
+a **= 1 # a = a **1
+puts a  # We can aplly tha same with every mathematical operator
