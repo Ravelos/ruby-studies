ec830079edbaffb0317c0a29a05d9c10b0d0e4c1
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-08-07 13:45:21 -0300

    logical operators && , ||, !

--- 7-logical_operators.rb
+++ 7-logical_operators.rb
@@ -1,1 +1,30 @@
-#Logical operators 
+#Logical operators
+# We can use the following logical operators: && or and .
+
+v1 = 34
+v2 = 57
+v3 = 50
+v4 = 86
+
+if (v1 < v2) && (v3 < v4)
+    puts "Attended condition in both cases (and)"
+else
+    puts "Unattended condition in both cases"
+end
+
+
+# Using 'or' which can be replace by '||' or viceversa.
+
+if (v1 < v2) ||  (v3 < v4)
+    puts "At least one condition was attended (or)"
+else
+    puts "Unattended condition in both cases"
+end
+
+# Using '!' . It needs only one element.
+
+if !(v3 < v4)
+    puts "Attended denial"
+else
+    puts "Unattended denial"
+end
