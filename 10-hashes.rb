1d180e526f0253ff865cd26e70143e5e0e7ec770
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-08-07 14:53:35 -0300

    hashes

---
+++ 10-hashes.rb
@@ -0,0 +1,16 @@
+#Hashes they have a key value like this "12"=>"123". They are declare with curly braces
+
+h = {"a" => "123", "b" => "456"}
+
+puts h
+
+# You can show the value according to the key
+puts h["a"]
+
+# There is another way to declare the hashes
+
+g = {a:"457",b:"098"}
+
+puts g
+puts g[:a]
+puts g[:b]
