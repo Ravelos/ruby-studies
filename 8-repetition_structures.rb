c2cdd84ef606bb039a71d25c5fbbc657151daa16
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-08-07 14:22:41 -0300

    loops while and until

---
+++ 8-repetition_structures.rb
@@ -0,0 +1,16 @@
+#Repetition structures while , until
+i = 1
+
+while i <= 50 # While it is true
+    puts"Oswaldo -#{i}"
+    i += 1
+end
+# i == 50
+#Until test while it is false
+
+until i <= 0
+    puts "I'm false -#{i}"
+    i -= 1
+end
+
+puts "The end!"
