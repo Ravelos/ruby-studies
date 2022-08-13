41ea1f5ad57a22d7f459f9218a6f951b746b5fbb
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-08-12 22:10:24 -0300

    classes

---
+++ 13-classes.rb
@@ -0,0 +1,14 @@
+#How to declare a class
+
+class Conta
+    #### Your method goes here
+end
+
+#If we want to use that class on irb we just type require_relative and the name of the file .
+# The covention is to start the class with capital letters and if it is composed we must use camel case
+# Creating an object from that class:
+
+
+Obj1 = Conta.new
+
+Obj1.class
