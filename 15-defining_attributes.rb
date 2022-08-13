03267fc42ff374d90773c1ff1cfd9d0a3e4ffc70
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-08-13 11:36:20 -0300

    another convention

--- 15-defining_attributes.rb
+++ 15-defining_attributes.rb
@@ -2,6 +2,26 @@ class Person
     @age = nil
     @name = nil
 
+    def name= (name)
+        @name = name
+    end
+
+    def age= (age)
+        @age = age
+    end
+
+    def name
+        @name
+    end
+
+    def age
+        @age
+    end
+
+end
+
+# java convention
+=begin
     def set_name(name)
         @name = name
     end
@@ -21,6 +41,7 @@ class Person
 end
 
 
+
 ################################
 # We are setting the attributes' values and for showing them we are going to use puts
 
@@ -37,3 +58,19 @@ puts first_person.get_age
 
 puts second_person.get_name
 puts second_person.get_age
+
+=end
+
+first_person = Person.new
+first_person.name ="Ozzy"
+first_person.age = 37
+
+second_person = Person.new
+second_person.name = "Venelope"
+second_person.age =33
+
+puts first_person.name
+puts first_person.age
+
+puts second_person.name
+puts second_person.age
