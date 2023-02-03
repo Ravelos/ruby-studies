635b75fefb7dcede5a5ec265606b85697c8cafbe
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-02 23:15:37 -0300

    creates the 2nd mission for this class

---
+++ only_ruby/third-class/second_mission.rb
@@ -0,0 +1,2 @@
+puts 'Missao 2 - Crie uma collection do tipo Hash e permita que o usuário crie três elementos informando a chave e o valor. No final do programa para cada um desses elementos imprima a frase “Uma das chaves é **** e o seu valor é ****.'
+
