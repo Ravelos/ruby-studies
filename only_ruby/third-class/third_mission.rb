176e327eccf9efccdc2743ccb303a24cf7c899e8
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-02 23:15:53 -0300

    creates the 3rd mission for this class

---
+++ only_ruby/third-class/third_mission.rb
@@ -0,0 +1,3 @@
+puts 'Missao 3 - Dado o seguinte hash:
+Numbers = {a: 10, b: 30, c: 20, d: 25, e: 15}
+Crie uma instrucao que seleciona o maior valor deste hash e no final imprima a chave e valor do elemento resultante'
