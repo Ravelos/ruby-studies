bdf189aa944d2534b874cdc29f9b27e72b9727d6
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-06 17:51:01 -0300

    refactors the calculator

--- only_ruby/second-class/first_mission.rb
+++ only_ruby/second-class/first_mission.rb
@@ -1,45 +1,69 @@
 puts 'First Mission: Using conditions and iteration structures, make a calculator that offers the user the options to multiply, divide, adds or substract two numbers. Do not forget to allow the user to close the program'
 
-def choosing
+def divide(n1,n2)
+    result = n1 / n2
+    puts "#{n1} divided by #{n2} is: #{result}"
+end
+
+def multiply(n1,n2)
+    result = n1 * n2
+    puts "#{n1} times #{n2} is: #{result}"
+end
+
+def addition(n1,n2)
+    result = n1 + n2
+    puts "#{n1} plus #{n2} is: #{result}"
+end
+
+def substraction(n1,n2)
+    result = n1 - n2
+    puts "#{n1} minus #{n2} is: #{result}"
+end
+
+def calculator
     puts 'Choose an option:'
     puts '1 Division'
     puts '2 Multiplication'
-    puts '3 Addtition'
+    puts '3 Addition'
     puts '4 Substraction'
+    puts 'If you do not want to use the calculator type any other option'
     option = gets.chomp.to_i
-end
-
-def verify_options
-    if option.class = t.string
-        puts 'Type a number according to the options given: '
-        choosing()
-    end
-end
-
-def calculating
-
-    choosing()
-    verify_options()
-
-    puts 'Now type your first number:'
-    n1 = gets.chomp.to_i
-    puts 'Now type your second number:'
-    n2 = gets.chomp.to_i
 
     if option == 1
-        result = n1 / n2
-        puts "#{n1} divided by #{n2} is: #{result}"
+        puts "Type your first number:"
+        n1 = gets.chomp.to_f
+        puts "Type your second number:"
+        n2 = gets.chomp.to_f
+
+        divide(n1,n2)  
+
     elsif option == 2
-        result = n1 * n2
-        puts "#{n1} times #{n2} is: #{result}"
+        puts "Type your first number:"
+        n1 = gets.chomp.to_f
+        puts "Type your second number:"
+        n2 = gets.chomp.to_f 
+
+        multiply(n1,n2)       
+    
     elsif option == 3
-        result = n1 + n2
-        puts "#{n1} plus #{n2} is: #{result}"
+        puts "Type your first number:"
+        n1 = gets.chomp.to_f
+        puts "Type your second number:"
+        n2 = gets.chomp.to_f 
+
+        addition(n1,n2)       
+    
+    elsif option == 4
+        puts "Type your first number:"
+        n1 = gets.chomp.to_f
+        puts "Type your second number:"
+        n2 = gets.chomp.to_f 
+
+        substraction(n1,n2)
+    
     else
-        result = n1 - n2
-        puts "#{n1} minus #{n2} is : #{result}"
+        puts "Your option is invalid"        
     end
 end
 
-
-calculating()
+calculator()
