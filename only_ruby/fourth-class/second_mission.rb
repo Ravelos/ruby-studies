96d654afab1f230cdc175c7ca21d7bb98951c7e5
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-18 20:31:30 -0300

    creates the fourth mission file

--- only_ruby/fourth-class/second_mission.rb
+++ only_ruby/fourth-class/second_mission.rb
@@ -1,4 +1,5 @@
 puts 'Missao 2 - Siga a documentacao da gem cpf_cnpj para criar um programa que recebe como
 entrada um numero de cpf e em um metodo verifique se este numero e valido.
 Link da documntacao:
+
 https://github.com/fnando/cpf_cnpj'
